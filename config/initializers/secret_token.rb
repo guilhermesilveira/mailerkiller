d0e5c7344a465dc2d9336fe8592dbb5ab6a3ee6d
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2012-05-04 13:10:44 -0300

    first attempt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailerkiller::Application.config.secret_token = '75f7ad1c7122120d972879441f3ba4fca6e66a604c95e503a4d3a931a8858e942f97d6925cd1dc07795d8ce8285c80d589944ef2eac62b1946e9747c7959b24d'
